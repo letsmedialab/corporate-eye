bcfde544cf43a54f04062a56e137a2607688ba63
Pranav <pranav.ji@live.in>
2022-08-06 02:51:54 +0100

    aug 6th

--- NativeSupportServiceApplication/NativeSupportServiceApplication/Program.cs
+++ NativeSupportServiceApplication/NativeSupportServiceApplication/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using NativeSupportServiceApplication.Modules;
+
+namespace NativeSupportServiceApplication
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Console.WriteLine("sfas");
+            // To customize application configuration such as set high DPI settdfgfdgings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            
+            
+            Application.Run(new LoadingSplash());
+          
+            //Console.WriteLine("sfas");
+           
+        }
+    }
+}
